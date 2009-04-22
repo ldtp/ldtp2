a58f17378e66d98518ff09a588c032ad36f95b25
Eitan Isaacson <eitan@sparky.(none)>
2009-04-22 20:25:27 +0300

    turn caching on

--- ldtpd/__init__.py
+++ ldtpd/__init__.py
@@ -1,8 +1,10 @@
 from twisted.internet import reactor
 from twisted.web import server
 from daemon import Ldtpd
+import pyatspi
 
 def main(port=4118):
+    pyatspi.setCacheLevel(pyatspi.CACHE_PROPERTIES)
     r = Ldtpd()
     reactor.listenTCP(port, server.Site(r))
     reactor.run()
