c60562edb356fd215df37cb9a54de2cb47724224
Eitan Isaacson <eitan@sparky.(none)>
2009-04-29 05:11:25 +0300

    made ooldtp introspective and awesome. still needs plenty of optimization

--- ooldtp/__init__.py
+++ ooldtp/__init__.py
@@ -1,4 +1,7 @@
-from _context import Context
+from _context import Context, Component
 
 def context(window_name):
     return Context(window_name)
+
+def component(window_name, object_name):
+    return Component(window_name, object_name)
--- ooldtp/_context.py
+++ ooldtp/_context.py
@@ -1,17 +1,85 @@
 import ldtp
 
-class Context:
+class _Wrapper(object):
+    def __new__(cls, *args, **kwargs):
+        if not cls._generated:
+            cls._generated = True
+            d = ldtp.__dict__
+            cls._wrapped_methods =[]
+            for attr in d:
+                if cls._isRemoteMethod(d[attr]) and cls._isRelevant(d[attr]):
+                    cls._wrapped_methods.append(attr)
+                    setattr(cls, attr, d[attr])
+        return object.__new__(cls)
+
+    def __getattribute__(self, name):
+        obj = object.__getattribute__(self, name)
+        if name in object.__getattribute__(self, '_wrapped_methods'):
+            wrapper = object.__getattribute__(self, '_wrapMethod')
+            return wrapper(obj)
+        return obj
+
+    def _isRemoteMethod(cls, obj):
+        return hasattr(obj, '_Method__name')
+    _isRemoteMethod = classmethod(_isRemoteMethod)
+
+    def _listArgs(cls, func):
+        l = [a.strip() for a in func.__doc__.split('\n')]
+        l = filter(lambda x: x.startswith('@param '), l)
+        l = [x[7:].split(':')[0] for x in l]
+        return l
+    _listArgs = classmethod(_listArgs)
+
+class Context(_Wrapper):            
+    _generated = False
+
     def __init__(self, window_name):
         self._window_name = window_name
-    def __getattr__(self, name):
-        obj = getattr(ldtp, name)
-        if callable(obj):
-            return _ContextFuncWrapper(self._window_name, obj)
-        raise AttributeError
 
+    def _wrapMethod(self, obj):
+        return _ContextFuncWrapper(self._window_name, obj)
+
+    def _isRelevant(cls, obj):
+        args = cls._listArgs(obj)
+        return args and 'window_name' == args[0]
+    _isRelevant = classmethod(_isRelevant)
+    
 class _ContextFuncWrapper:
     def __init__(self, window_name, func):
         self._window_name = window_name
         self._func = func
+
     def __call__(self, *args, **kwargs):
         return self._func(self._window_name, *args, **kwargs)
+
+class Component(_Wrapper):            
+    _generated = False
+
+    def __init__(self, window_name, object_name):
+        self._window_name = window_name
+        self._object_name = object_name
+
+    def _wrapMethod(self, func):
+        return _ComponentFuncWrapper(
+            self._window_name, self._object_name, func)
+
+    def _isRelevant(cls, obj):
+        args = cls._listArgs(obj)
+        return len(args) >= 2 and \
+            'window_name' == args[0] and 'object_name' == args[1]
+    _isRelevant = classmethod(_isRelevant)
+    
+class _ComponentFuncWrapper:
+    def __init__(self, window_name, object_name, func):
+        self._window_name = window_name
+        self._object_name = object_name
+        self._func = func
+
+    def __call__(self, *args, **kwargs):
+        return self._func(
+            self._window_name, self._object_name, *args, **kwargs)
+
+if __name__ == "__main__":
+    c = Component('Calculator', 'btnNumeric1')
+    print dir(c)
+    c.click()
