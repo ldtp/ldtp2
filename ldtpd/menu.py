818919599b6006fa183f26f0af12bad7c15d2526
nags <nags@nags-desktop.(none)>
2009-05-18 00:40:26 -0700

    Included license

--- ldtpd/menu.py
+++ ldtpd/menu.py
@@ -1,6 +1,27 @@
+'''
+LDTP v2 Core.
+
+@author: Eitan Isaacson <eitan@ascender.com>
+@author: Nagappan Alagappan <nagappan@gmail.com>
+@copyright: Copyright (c) 2009 Eitan Isaacson
+@copyright: Copyright (c) 2009 Nagappan Alagappan
+@license: LGPL
+
+http://ldtp.freedesktop.org
+
+This file may be distributed and/or modified under the terms of the GNU General
+Public License version 2 as published by the Free Software Foundation. This file
+is distributed without any warranty; without even the implied warranty of 
+merchantability or fitness for a particular purpose.
+
+See "COPYING" in the source distribution for more information.
+
+Headers in this file shall remain intact.
+'''
+
+import re
 import pyatspi 
 from utils import Utils
-import re
 
 class Menu(Utils):
     def selectmenuitem(self, window_name, object_name):
