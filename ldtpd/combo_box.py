c6f78e9771faa522561e562b6be85e8034a4e651
nags <nags@nags-desktop.(none)>
2009-05-23 00:04:52 -0700

    Implemented new API verifydropdown, verifyshowlist, verifyhidelist, verifyselect

--- ldtpd/combo_box.py
+++ ldtpd/combo_box.py
@@ -218,3 +218,125 @@ class ComboBox(Utils):
             self._click_object(obj, 'press')
 
         return 1
+
+    def verifydropdown(self, window_name, object_name):
+        '''
+        Verify drop down list / menu poped up
+        
+        @param window_name: Window name to type in, either full name,
+        LDTP's name convention, or a Unix glob.
+        @type window_name: string
+        @param object_name: Object name to type in, either full name,
+        LDTP's name convention, or a Unix glob. 
+        @type object_name: string
+
+        @return: 1 on success.
+        @rtype: integer
+        '''
+        obj = self._get_object(window_name, object_name)
+        self._grab_focus(obj)
+
+        child_obj = self._get_combo_child_object_type(obj)
+        if not child_obj:
+            raise LdtpServerException('Unable to get combo box children')
+
+        if child_obj == pyatspi.ROLE_LIST and \
+                self._check_state(obj, pyatspi.STATE_FOCUSABLE):
+                return 1
+        elif child_obj == pyatspi.ROLE_MENU:
+            if self._check_state(obj, pyatspi.STATE_VISIBLE) and \
+                    self._check_state(obj, pyatspi.STATE_SHOWING):
+                return 1
+        return 0
+
+    def verifyshowlist(self, window_name, object_name):
+        '''
+        Verify drop down list / menu poped up
+        
+        @param window_name: Window name to type in, either full name,
+        LDTP's name convention, or a Unix glob.
+        @type window_name: string
+        @param object_name: Object name to type in, either full name,
+        LDTP's name convention, or a Unix glob. 
+        @type object_name: string
+
+        @return: 1 on success.
+        @rtype: integer
+        '''
+        return self.verifydropdown(window_name, object_name)
+
+    def verifyhidelist(self, window_name, object_name):
+        '''
+        Verify list / menu is hidden in combo box
+        
+        @param window_name: Window name to type in, either full name,
+        LDTP's name convention, or a Unix glob.
+        @type window_name: string
+        @param object_name: Object name to type in, either full name,
+        LDTP's name convention, or a Unix glob. 
+        @type object_name: string
+
+        @return: 1 on success.
+        @rtype: integer
+        '''
+        obj = self._get_object(window_name, object_name)
+        self._grab_focus(obj)
+
+        child_obj = self._get_combo_child_object_type(obj)
+        if not child_obj:
+            raise LdtpServerException('Unable to get combo box children')
+
+        if child_obj == pyatspi.ROLE_LIST and \
+                not self._check_state(obj, pyatspi.STATE_FOCUSABLE):
+                return 1
+        elif child_obj == pyatspi.ROLE_MENU:
+            if not self._check_state(obj, pyatspi.STATE_VISIBLE) and \
+                    not self._check_state(obj, pyatspi.STATE_SHOWING):
+                return 1
+        return 0
+
+    def verifyselect(self, window_name, object_name, item_name):
+        '''
+        Verify the item selected in combo box
+        
+        @param window_name: Window name to type in, either full name,
+        LDTP's name convention, or a Unix glob.
+        @type window_name: string
+        @param object_name: Object name to type in, either full name,
+        LDTP's name convention, or a Unix glob. 
+        @type object_name: string
+        @param item_name: Item name to select
+        @type object_name: string
+
+        @return: 1 on success.
+        @rtype: integer
+        '''
+        obj = self._get_object(window_name, object_name)
+        self._grab_focus(obj)
+
+        child_obj = self._get_combo_child_object_type(obj)
+        if not child_obj:
+            raise LdtpServerException('Unable to get combo box children')
+        if child_obj.getRole() == pyatspi.ROLE_LIST:
+            for child in self._list_objects (child_obj):
+                if child == child_obj:
+                    # As the _list_objects gives the current object as well
+                    # ignore it
+                    continue
+                try:
+                    texti = child.queryText()
+                    text = texti.getText(0, texti.characterCount)
+                except NotImplementedError:
+                    text = child.name
+
+                if self._glob_match(item_name, text):
+                    return 1
+        elif child_obj.getRole() == pyatspi.ROLE_MENU:
+            for child in self._list_objects (child_obj):
+                if child == child_obj:
+                    # As the _list_objects gives the current object as well
+                    # ignore it
+                    continue
+                if self._glob_match(item_name, child.name):
+                    return 1
+        return 0
