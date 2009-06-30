cefd38fb1a7cfcf462beaedd0e6fa8f0c12b0cd1
Eitan Isaacson <eitan@ascender.com>
2009-06-30 14:36:06 -0700

    added file-out logging

--- ldtp/log.py
+++ ldtp/log.py
@@ -2,16 +2,22 @@ from os import environ as env
 import logging
 
 AREA = 'ldtp.client'
+
 ENV_LOG_LEVEL = 'LDTP_LOG_LEVEL'
+ENV_LOG_OUT = 'LDTP_LOG_OUT'
 
 log_level = getattr(logging, env.get(ENV_LOG_LEVEL, 'NOTSET'), logging.NOTSET)
 
 logger = logging.getLogger('ldtp.client')
 
-handler = logging.StreamHandler()
-
-handler.setFormatter(
-    logging.Formatter('%(name)-11s: %(levelname)-8s %(message)s'))
+if not env.has_key(ENV_LOG_OUT):
+    handler = logging.StreamHandler()
+    handler.setFormatter(
+        logging.Formatter('%(levelname)-8s %(message)s'))
+else:
+    handler = logging.FileHandler(env[ENV_LOG_OUT])
+    handler.setFormatter(
+        logging.Formatter('%(asctime)s %(levelname)-8s %(message)s'))
 
 logger.addHandler(handler)
 
