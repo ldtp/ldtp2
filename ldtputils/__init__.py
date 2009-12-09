2d518bee461a4fa7a4b1c779a90b7cc7d84b9ff2
nags <nags@nags-desktop.(none)>
2009-12-09 09:00:24 -0800

    Alignment fix

--- ldtputils/__init__.py
+++ ldtputils/__init__.py
@@ -1,18 +1,36 @@
+'''
+LDTP v2 ldtputils init file
+
+@author: Eitan Isaacson <eitan@ascender.com>, Nagappan Alagappan <nagappan@gmail.com>
+@copyright: Copyright (c) 2009 Eitan Isaacson, Nagappan Alagappan
+@license: LGPL
+
+http://ldtp.freedesktop.org
+
+This file may be distributed and/or modified under the terms of the GNU General
+Public License version 2 as published by the Free Software Foundation. This file
+is distributed without any warranty; without even the implied warranty of 
+merchantability or fitness for a particular purpose.
+
+See "COPYING" in the source distribution for more information.
+
+Headers in this file shall remain intact.
+'''
 from ldtp import imagecapture
 
-def imagecompare (imgfile1, imgfile2):
+def imagecompare(imgfile1, imgfile2):
     try:
         import ImageChops, Image
     except ImportError:
         raise Exception, 'Python-Imaging package not installed'
     try:
         diffcount = 0.0
-        im1 = Image.open (imgfile1)
-        im2 = Image.open (imgfile2)
+        im1 = Image.open(imgfile1)
+        im2 = Image.open(imgfile2)
 
-        imgcompdiff = ImageChops.difference (im1, im2)
-        diffboundrect = imgcompdiff.getbbox ()
-        imgdiffcrop = imgcompdiff.crop (diffboundrect)
+        imgcompdiff = ImageChops.difference(im1, im2)
+        diffboundrect = imgcompdiff.getbbox()
+        imgdiffcrop = imgcompdiff.crop(diffboundrect)
 
         data = imgdiffcrop.getdata()
 
@@ -20,7 +38,7 @@ def imagecompare (imgfile1, imgfile2):
         for row in data:
             seq += list(row)
 
-        for i in xrange (0, imgdiffcrop.size[0] * imgdiffcrop.size[1] * 3, 3):
+        for i in xrange(0, imgdiffcrop.size[0] * imgdiffcrop.size[1] * 3, 3):
             if seq[i] != 0 or seq[i+1] != 0 or seq[i+2] != 0:
                 diffcount = diffcount + 1.0
         
