c5d8ad62d844f271546b22efd244c5dd64bb97a4
nags <nags@nags-desktop.(none)>
2010-03-08 23:31:48 -0800

    Added Simulate mouse move new API

--- ldtpd/mouse.py
+++ ldtpd/mouse.py
@@ -134,3 +134,61 @@ class Mouse(Utils):
         return self._mouse_event(_coordinates.x + _coordinates.width / 2,
                                  _coordinates.y + _coordinates.height / 2,
                                  'b1d')
+
+    def simulatemousemove(self, source_x, source_y, dest_x, dest_y):
+        """
+        @param source_x: Source X
+        @type source_x: integer
+        @param source_y: Source Y
+        @type source_y: integer
+        @param dest_x: Dest X
+        @type dest_x: integer
+        @param dest_y: Dest Y
+        @type dest_y: integer
+
+        @return: 1 if simulation was successful, 0 if not.
+        @rtype: integer
+        """
+        size = self._get_size(self._desktop)
+        if (source_x < size.x or source_y < size.y or \
+                dest_x > size.width or dest_y > size.height) and \
+                (source_x > size.width or source_y > size.height or \
+                     dest_x < size.x or dest_y < size.y):
+            return 0
+
+        x_flag = False # Iterated x ?
+        y_flag = False # Iterated y ?
+        while True:
+            if not x_flag:
+                if source_x > dest_x:
+                    # If source X greather than dest X
+                    # then move -1 pixel
+                    source_x -= 1
+                elif source_x < dest_x:
+                    # If source X less than dest X
+                    # then move +1 pixel
+                    source_x += 1
+                else:
+                    # If source X equal to dest X
+                    # then don't process X co-ordinate
+                    x_flag = True
+            if not y_flag:
+                if source_y > dest_y:
+                    # If source Y greather than dest Y
+                    # then move -1 pixel
+                    source_y -= 1
+                elif source_y < dest_y:
+                    # If source Y less than dest Y
+                    # then move +1 pixel
+                    source_y += 1
+                else:
+                    # If source Y equal to dest Y
+                    # then don't process Y co-ordinate
+                    y_flag = True
+            # Start mouse move from source_x, source_y to dest_x, dest_y
+            self.generatemouseevent(source_x, source_y, 'abs')
+            if source_x == dest_x and source_y == dest_y:
+                # If we have reached the dest_x and dest_y
+                # then break the loop
+                break
+        return 1
